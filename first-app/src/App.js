2eaa2e7397ca1209da4175869be8aaaabca34380
Knieciu <gabennewell00@gmail.com>
2023-10-19 16:57:10 +0200

    did some basic form for adding todo's

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
     <div className="todoApp">
       <h1>Todo List</h1>
+      <TodoForm/>
     </div>
   );
 }
