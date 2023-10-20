4694317ac830afbd48a605171014277928a4ce6d
Knieciu <gabennewell00@gmail.com>
2023-10-20 23:05:53 +0200

    Change code

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,12 +1,32 @@
 
 import './App.css';
-import TodoList from './components/TodoList';
+import React, {useState} from 'react';
 
 function App() {
+const [newItem, setNewItem] = useState("");
+
+const writingItem = e =>{
+  e.preventDefault();
+
+  setNewItem(e.target.value);
+};
+
+
+
   return (
     <div className="todoApp" >
+
+
       <h1>Todo List</h1>
-      <TodoList />
+      <input
+        className="todoInput"
+        placeholder="Add todo"
+        value={newItem}  
+        onChange={writingItem}
+        />
+          
+        
+      <button className="todoButton">Add</button>
     </div>
   );
 }
