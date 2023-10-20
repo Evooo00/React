f39dccf5cb1d4aa44f926807aa7fd76ab7c3982d
Knieciu <gabennewell00@gmail.com>
2023-10-20 23:41:53 +0200

    Adding and deleting

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -5,6 +5,11 @@ import React, {useState} from 'react';
 function App() {
 const [newItem, setNewItem] = useState("");
 
+//Creating array for holding all items
+const [items, setItems] = useState([]);
+//------------------------------------
+
+
 const writingItem = e =>{
   e.preventDefault();
 
@@ -13,20 +18,57 @@ const writingItem = e =>{
 
 
 
+function addItem(){
+  //console.log("test");
+  if (!newItem){
+    return;
+  }
+
+  const item = {
+    id: Math.floor(Math.random() * 1000000),
+    value: newItem
+  }
+
+  setItems(oldList => [...oldList, item]);
+  setNewItem("");
+  
+  // console.log(items);
+
+}
+
+
+function deleteItem(id){
+  // console.log(id);
+
+  const array = items.filter(item => item.id !== id);
+  setItems(array);
+}
+
   return (
     <div className="todoApp" >
 
 
       <h1>Todo List</h1>
-      <input
-        className="todoInput"
-        placeholder="Add todo"
-        value={newItem}  
-        onChange={writingItem}
-        />
+      <div>
+        <input
+          className="todoInput"
+          placeholder="Add todo"
+          value={newItem}  
+          onChange={writingItem}
+          />
+            
           
-        
-      <button className="todoButton">Add</button>
+        <button className="todoButton" onClick={addItem}>Add</button>
+      </div>
+      <ul>
+        {items.map(item =>{
+          return(
+            <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)}>X</button></li>
+          )
+        })}
+
+      </ul>
+
     </div>
   );
 }
