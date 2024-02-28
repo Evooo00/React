8e63a558c2376e4ae3d75fb7f934a3b99839e0b5
Knieciu <gabennewell00@gmail.com>
2024-02-28 12:29:58 +0100

    calculator structure

---
+++ calculator/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="result">15</div>
+
+      <button className="clear">AC</button>
+      <button className="divide">/</button>
+
+      <button className="number7 number">7</button>
+      <button className="number8 number">8</button>
+      <button className="number9 number">9</button>
+      <button className="multiplication">X</button>
+
+      <button className="number4 number">4</button>
+      <button className="number5 number">5</button>
+      <button className="number6 number">6</button>
+      <button className="minus">-</button>
+
+      <button className="number1 number">1</button>
+      <button className="number2 number">2</button>
+      <button className="number3 number">3</button>
+      <button className="plus">+</button>
+
+      <button className="number0 number">0</button>
+      <button className="dot number">.</button>
+      <button className="equal">=</button>
+    </div>
+  );
+}
+
+export default App;
